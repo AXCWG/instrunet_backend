2822ed1f6f153290e2ea9cae2fbe4026dae03dd2
AXCWG <xiey0@qq.com>
2024-12-03 12:00:23 +0800

    Attaching with UVR

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./database.db');
 const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
-
+const {spawn} = require("child_process");
+const nrc = require('node-run-cmd')
+console.log(process.cwd())
 app.use(bodyParser.json({"limit": "50mb"}));
 app.use(express.json());
 
@@ -13,11 +15,25 @@ db.close()
 
 
 app.post('/submit', function (req, res) {
-    fetch(req.body.file).then(res=>{
-        res.arrayBuffer() .then(r => fs.writeFileSync("./file", Buffer.from(r) ));
+    fetch(req.body.file).then(res => {
+        res.arrayBuffer().then(r => fs.writeFileSync("./audio_file", Buffer.from(r)));
     })
     res.header("Access-Control-Allow-Origin", "*");
+
+    var callback = function (d) {
+        console.log(d.toString());
+    }
+    var errcb = function (d) {
+        console.log(d.toString());
+    }
+
+    nrc.run("python separate.py -m MDX_Net_Models/UVR_MDXNET_KARA.onnx -d -c 4000 ./audio_file", {
+        onData: callback,
+        onError: errcb
+    });
+
     res.end("api_success");
+
 
 })
 app.options('/submit', function (req, res) {
